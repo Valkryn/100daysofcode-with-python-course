d90538bbccdb09c767d9b5e8dc859f2e02066e81
Calvin Gutierrez <calvin.gutierrez@einsteinmed.edu>
2023-11-16 16:24:45 -0500

    added try catch

--- days/01-03-datetimes/challenge_pomodoro.py
+++ days/01-03-datetimes/challenge_pomodoro.py
@@ -3,12 +3,22 @@ import time
 
 
 def main():
+    while True:
+        try:
+            user_input = int(input('How many minutes to focus for?: '))
+            break
+        except ValueError:
+            print('please input a number')
     seconds_1 = timedelta(seconds=1)
-    seconds_20 = timedelta(seconds=20*60)
-    while seconds_20 != timedelta(seconds=0):
-        print(seconds_20)
+    study_time = timedelta(seconds=user_input * 60)
+    pomodoro(study_time, seconds_1)
+
+
+def pomodoro(study_time, seconds_1):
+    while study_time != timedelta(seconds=0):
+        print(study_time)
         time.sleep(1)
-        seconds_20 = seconds_20 - seconds_1
+        study_time = study_time - seconds_1
     print('times up!')
 
 
